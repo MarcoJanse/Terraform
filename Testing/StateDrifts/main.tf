0883d61fd8cdce8ccc7f369eb9b95b8baa2432c4
Marco Janse <mjanse@thefactory.nl>
2022-02-15 08:16:37 +0100

    syntax

--- Testing/StateDrifts/main.tf
+++ Testing/StateDrifts/main.tf
@@ -1,3 +1,7 @@
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "general-rg" {
   location = var.azure_region
   name     = "${var.cust}-${var.app_name}-${var.environment}-rg"
@@ -5,20 +9,20 @@ resource "azurerm_resource_group" "general-rg" {
 }
 
 resource "azurerm_virtual_network" "workspace" {
-  name                = "${var.app-name}-${var.environment}-vnet"
+  name                = "${var.app_name}-${var.environment}-vnet"
   location            = azurerm_resource_group.general-rg.location
   resource_group_name = azurerm_resource_group.general-rg.name
-  address_space       = ["10.252.${var.addr-space}.0/23"]
+  address_space       = ["10.252.${var.address_space}.0/23"]
   dns_servers         = ["10.252.3.4", "10.254.3.5"]
 
   tags = var.default_tags
 }
 
 resource "azurerm_subnet" "workspace_subnet" {
-  name                 = "${var.app-name}-${var.environment}-workspace-subnet"
+  name                 = "${var.app_name}-${var.environment}-workspace-subnet"
   resource_group_name  = azurerm_resource_group.general-rg.name
   virtual_network_name = azurerm_virtual_network.workspace.name
-  address_prefix       = "10.252.${var.addr-space}.0/24"
+  address_prefixes     = ["10.252.${var.address_space}.0/24"]
 }
 
 resource "random_id" "randomId" {
@@ -32,7 +36,7 @@ resource "random_id" "randomId" {
 resource "azurerm_storage_account" "sharedStorage" {
   name                      = "input${lower(random_id.randomId[count.index].hex)}"
   resource_group_name       = azurerm_resource_group.general-rg.name
-  location                  = var.azure-region
+  location                  = var.azure_region
   account_tier              = "Standard"
   account_kind              = "StorageV2"
   account_replication_type  = "LRS"
