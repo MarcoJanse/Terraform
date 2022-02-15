4ca720e804e77a7429db5a40a6859fe6f8dfcc71
Marco Janse <mjanse@thefactory.nl>
2022-02-15 15:53:02 +0100

    version upgrades

--- Testing/StateDrifts/_default.tf
+++ Testing/StateDrifts/_default.tf
@@ -1,12 +1,12 @@
 terraform {
 
-  required_version = "= 0.14.11"
+  required_version = "= 1.1.2"
   # Initial version to test config drift with on 07/02/2022: 0.14.11
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+      version = "=2.96.0"
     }
   }
 }
